b5b6b89a35ee185d6abfc832bde7d47d9d7943eb
Ben Kelly <bk@ancilla.ca>
2009-12-13 19:52:16 -0500

    'args' support - needed by previous commit, oops

--- ast/IO.lua
+++ ast/IO.lua
@@ -1,12 +1,15 @@
 local io = require ((...):gsub("ast%.IO$", "io"))
 
 return function(name, width)
+    local args = { select(2, io(name, "width", width)) }
     width = io(name, "width", width)
+    
     
     local IO = {
         tag = "io";
         width = width;
         len = io(name, "hasvalue") and 1 or 0;
+        args = args;
         type = name;
     }
     
@@ -15,12 +18,12 @@ return function(name, width)
     end
     
     function IO:unpack(fd, buf)
-        return io(name, "unpack", fd, buf, width)
+        return io(name, "unpack", fd, buf, width, unpack(args))
     end
     
     function IO:pack(fd, data, key)
         --print("IO-pack", data, key, data[key], "!!", unpack(data))
-        return io(name, "pack", fd, data[key], width)
+        return io(name, "pack", fd, data[key], width, unpack(args))
     end
     
     return IO
