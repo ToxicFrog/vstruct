f15a25aad6c1d1dcd09978e487e431019bc5ce71
Ben Kelly <bk@ancilla.ca>
2009-12-18 19:39:48 -0500

    fixed point support

--- io/p.lua
+++ io/p.lua
@@ -5,15 +5,21 @@ local p = {}
 function p.width(int, frac)
     assert( (int+frac) % 8 == 0, "fixed point number is not byte-aligned")
     
-    return int+frac
+    return (int+frac)/8
 end
 
-function p.unpack(...)
-    return 0.0
+function p.unpack(fd, buf, int, frac)
+    int = tonumber(int)
+    frac = tonumber(frac)
+
+    return io("i", "unpack", fd, buf, (int+frac)/8)/(2^frac)
 end
 
-function p.pack(...)
-    return io("x", "pack", ...)
+function p.pack(fd, data, int, frac)
+    int = tonumber(int)
+    frac = tonumber(frac)
+    
+    return io("i", "pack", fd, data * 2^frac, (int+frac)/8)
 end
 
 return p
