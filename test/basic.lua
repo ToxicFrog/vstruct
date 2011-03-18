2e37d9e061f142e7f1495817c73988eace6b9f24
Ben Kelly <bkelly@uoguelph.ca>
2011-03-17 20:48:56 -0400

    New 'vstruct.test.common' module; 'vstruct.test.basic' overhauled.

--- test/basic.lua
+++ test/basic.lua
@@ -5,116 +5,73 @@
 -- test cases
 -- simple: read and write of each format, seeks, endianness controls
 -- complex: naming, tables, repetition, combinations of all of these,
---		nested tables, multi formats per string
+--      nested tables, multi formats per string
 -- non-obvious: seek past end of file
 -- error handling: seek past start, read past end, invalid widths,
---		non-isomorphic input tables
+--      non-isomorphic input tables
 
-local c = string.char
-local require,math,ipairs,string,tostring,print,os,pairs,unpack,table
-	= require,math,ipairs,string,tostring,print,os,pairs,unpack,table
+local test = require "vstruct.test.common"
 
-local struct = require "vstruct"
+local x = test.x
+local T = test.autotest
 
-local function check_bm(m)
-    return m[8] and m[7] and m[6] and m[5]
-	and m[4] and not m[3] and m[2] and not m[1]
+test.group "basic tests"
+
+T("true",       "> b8",     x"0000 0000 0000 0001", true)
+T("false",      "> b8",     x"0000 0000 0000 0000", false)
+
+T("unsigned",   "< u3",     x"FE FF FF", 2^24-2)
+T("signed",     "< i3",     x"FE FF FF", -2)
+
+T("c str",      "z",        "foobar\0baz", "foobar", "foobar\0")
+T("padded str", "z10",      "foobar\0baz", "foobar", "foobar\0\0\0\0")
+
+T("fixed str",  "s4",       "foobar", "foob", "foob")
+
+T("counted str","< c4",     x"06000000".."foobar", "foobar")
+
+T("bitmask",    "m1",       x"FA", {{ false, true, false, true, true, true, true, true }})
+
+T("skip/pad",   "x4u1",     x"00 00 00 00 02", 2)
+
+T("seek @",     "@2 u1x2",  x"00 00 02 00 00", 2)
+T("seek +",     "+2 u1x2",  x"00 00 02 00 00", 2)
+T("seek -",     "+4-2 u1x2",x"00 00 02 00 00", 2)
+
+T("little-endian", "< u2",  x"00 01", 256)
+T("big-endian", "> u2",     x"00 01", 1)
+
+if test.bigendian() then
+    T("host-endian","= u2", x"01 00", 256)
+else
+    T("host-endian","= u2", x"01 00", 1)
 end
 
-local tests = {
-    -- booleans
-    { raw = "\0\0\0\0\0\0\0\1"; format = "> b8"; val = true; },
-    --	{ raw = "\1\0\0\0\0\0\0\0"; format = "(b8)*1"; val = true; },
-    { raw = "\0\0\0\0\0\0\0\0"; format = "1*(b8)"; val = false; },
-    -- unsigned integers
-    { raw = "\254\255\255"; format = "< u3"; val = 2^24-2; },
-    -- signed integers
-    { raw = "\254\255\255"; format = "< i3"; val = -2 },
-    -- plain strings
-    { raw = "foobar"; format = "s4"; val = "foob"; },
-    -- counted strings
-    { raw = "\006\000\000\000foobar"; format = "< c4"; val = "foobar"; },
-    -- null terminated strings
-    { raw = "foobar\0baz"; format = "z"; val = "foobar"; },
-    { raw = "foobar\0baz"; format = "z10"; val = "foobar"; },
-    -- bitmasks
-    { raw = "\250"; format = "m1"; test = check_bm; },
-    -- skip/pad
-    { raw = "\0\0\0\0\2"; format = "x4u1"; val = 2 },
-    -- seek
-    { raw = "\0\0\2\0\0"; format = "@2 u1 x2"; val = 2 },
-    { raw = "\0\0\2\0\0"; format = "+2 u1 x2"; val = 2 },
-    { raw = "\0\0\2\0\0"; format = "+4 -2 u1 x2"; val = 2 },
-    -- endianness
-    { raw = "\0\1"; format = "< u2"; val = 256 },
-    { raw = "\0\1"; format = "> u2"; val = 1 },
-    { raw = "\1\1"; format = "= u2"; val = 257 },
-    -- bitpacks
-    { raw = c(0x0F, 0x00); format = "< [2| x4 u4 x8]"; val = 0 },
-    { raw = c(0x0F, 0x00); format = "< [2| u4 x12]"; val = 15 },
-    { raw = c(0x0F, 0x00); format = "> [2| x8 u4 x4]"; val = 15 },
-    { raw = c(0x0F, 0x00); format = "< [2| i4 x12]"; val = -1 },
-    { raw = c(0x0F, 0x00); format = "> [2| x8 i4 x4]"; val = -1 },
-    { raw = c(0xF0); format = "[1| x4 b4]"; val = true },
-    { raw = c(0x00); format = "[1| x4 b4]"; val = false },
-    { raw = c(0x0F,0xA0); format = "> [2| x4 m8 x4]"; test = check_bm },
+T("bitpack [>b]",   "> [2| x15 b1 ]",  x"00 01", true)
+T("bitpack [<b]",   "< [2| x15 b1 ]",  x"01 00", true)
+T("bitpack [>u]",   "> [2| 4*u4 ]",  x"12 34", {1,2,3,4})
+T("bitpack [<u]",   "< [2| 4*u4 ]",  x"12 34", {3,4,1,2})
+T("bitpack [>i]",   "> [2| 4*i4 ]",  x"12 EF", {1,2,-2,-1})
+T("bitpack [<i]",   "< [2| 4*i4 ]",  x"12 EF", {-2,-1,1,2})
+T("bitpack [>m]",   "> [2| m5 m3 x8 ]",  x"12 00", {{false,false,false,true,false},{false,true,false}})
+T("bitpack [<m]",   "< [2| m5 m3 x8 ]",  x"00 12", {{false,false,false,true,false},{false,true,false}})
+
+T("fixed point >",  "> p8,8", x"40 80", 64.5)
+T("fixed point <",  "< p8,8", x"40 80", -127.75)
+
+T("repetition", "> 4*u1", x"01 02 03 04", { 1, 2, 3, 4 })
+T("groups", "> 2*(u1 i1)", x"01 FF 02 FE", { 1, -1, 2, -2 })
+T("tables", "> 2*{ u1 i1 }", x"01 FF 02 FE", { { 1, -1 }, { 2, -2 } })
+T("names", "> coords:{ x:u1 y:u1 } coords.z:u1", x"01 02 03", { coords = { x = 1, y = 2, z = 3 } })
+
+T("UCS-2 z",  "> z,2",  x"0061 0062 0000 FFFF", "\0a\0b", x"0061 0062 0000")
+T("UCS-2 z8", "> z8,2", x"0061 0062 0000 FFFF", "\0a\0b", x"0061 0062 0000 0000")
+
+-- floats are handled by the fp-*endian test modules
+
     -- FIXME - names
     -- FIXME - tables
     -- FIXME - repetition
-    -- fixed point
-    { raw = "\1\128"; format = "> p8,8"; val = 1.5; },
-    { raw = "\2\192"; format = "> p8,8"; val = 2.75; },
-    -- floats
-    { raw = c(0x00, 0x00, 0x00, 0x00); format = "< f4"; val = 0.0; },
-    { raw = c(0x3f, 0x80, 0x00, 0x00); format = "> f4"; val = 1.0; },
-    { raw = c(0x00, 0x00, 0x80, 0x3f); format = "< f4"; val = 1.0; },
-    { raw = c(0x00, 0x00, 0x80, 0xbf); format = "< f4"; val = -1.0; },
-    { raw = c(0x00, 0x00, 0x80, 0x7f); format = "< f4"; val = math.huge; },
-    { raw = c(0x00, 0x00, 0x80, 0xff); format = "< f4"; val = -math.huge; },
-    { raw = c(0x00, 0x00, 0xc0, 0x7f); format = "< f4"; val = 0/0; test = function(v) return v ~= v end },
-    { raw = c(0x00, 0x00, 0xc0, 0xff); format = "< f4"; val = 0/0; test = function(v) return v ~= v end },
-    -- doubles
-    { raw = c(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00); format = "< f8"; val = 0.0; },
-    { raw = c(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x3f); format = "< f8"; val = 1.0; },
-    { raw = c(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0xbf); format = "< f8"; val = -1.0; },
-    { raw = c(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0x7f); format = "< f8"; val = math.huge; },
-    { raw = c(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf0, 0xff); format = "< f8"; val = -math.huge; },
-    { raw = c(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf8, 0x7f); format = "< f8"; val = 0/0; test = function(v) return v ~= v end },
-    { raw = c(0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf8, 0xff); format = "< f8"; val = 0/0; test = function(v) return v ~= v end },
-}
+    -- FIXME - grouping
 
-function od(str)
-    return str:gsub('.', function(c) return string.format("%02X ", c:byte()) end)
-end
-
-function check(test)
-    local val, pass, raw
-    local fmt = "%16.16s %33.33s%2.2s %17.17s %4.4s %3.3s"
-
-    val = struct.unpack(test.format, test.raw)[1]
-    pass = test.test
-        and test.test(val)
-        or val == test.val
-    print(fmt:format(test.format, od(test.raw), "=>", tostring(val), pass and "PASS" or "FAIL", pass and "" or "!!!"):sub(1,79))
-    if not pass then return end
-
-    raw = struct.pack(test.format, {val})
-    pass = raw == test.raw
-    -- if we have a failure, it might be because there are multiple valid on-disk forms
-    -- for example, a boolean can be any non-zero value, but we always write it back out as 1
-    -- so, re-read it using the same format and see if it matches
-    if not pass then
-        local new_val = struct.unpack(test.format, raw)[1]
-        pass = test.test and test.test(val) or new_val == test.val
-    end
-
-    print(fmt:format(test.format, od(raw), "<=", tostring(val), pass and "PASS" or "FAIL", pass and "" or " !!!"):sub(1,79))
-end
-
-struct.unpack('< @0 u4 m4 @4 { u4 u4 } @8 foo:{ z4 z4 i4 } 4*(u6)', string.rep('-', 100))
-
-for i,test in ipairs(tests) do
-    check(test)
-end
-
-os.exit(0)
+test.report()
---
+++ test/common.lua
@@ -0,0 +1,95 @@
+local vstruct = require "vstruct"
+
+local char,ord = string.char,string.byte
+
+local test = { results = {} }
+
+function test.x(str)
+	return (str:gsub("%X", ""):gsub("%x%x", function(b) return char(tonumber(b, 16)) end))
+end
+
+function test.od(str, sep)
+	local fmt = "%02X"..(sep or " ")
+    return (str:gsub('.', function(c) return fmt:format(c:byte()) end))
+end
+
+function test.eq(x, y)
+	if type(x) ~= type(y) then return false end
+	
+	if type(x) == 'table' then
+		for k,v in pairs(x) do
+			if not test.eq(v, y[k]) then return false end
+		end
+		for k,v in pairs(y) do
+			if not test.eq(v, x[k]) then return false end
+		end
+		return true
+	end
+
+	return x == y
+end
+
+function test.group(name)
+	local group = { name=name }
+	table.insert(test.results, group)
+	test.current_group = group
+end
+
+-- record the results of the test
+-- test is the name
+-- result is the boolean pass/fail
+-- message is an optional string, and will be displayed to the user as 
+-- "note" or "fail" depending on the value of result
+function test.record(name, result, data)
+	table.insert(test.current_group, { name=name, result=result, message=message, data=data })
+end
+
+function test.autotest(name, format, buffer, data, output)
+	local eq = test.eq
+	local record = test.record
+	
+	output = output or buffer
+	
+	if type(data) ~= "table" then data = {data} end
+	
+	local unpacked = vstruct.unpack(format, buffer)
+	local packed = vstruct.pack(format, unpacked)
+	
+	record(name.." (U )", eq(unpacked, data), unpack(unpacked))
+	record(name.." (UP)", eq(packed, output), test.od(packed))
+
+	local packed = vstruct.pack(format, data)
+	local unpacked = vstruct.unpack(format, packed)
+	
+	record(name.." (P )", eq(packed, output), test.od(packed))
+	record(name.." (PU)", eq(unpacked, data), unpack(unpacked))
+end
+
+function test.report()
+	for _,group in ipairs(test.results) do
+		local failed = 0
+		print("\t=== "..group.name.." ===")
+
+		for _,test in ipairs(group) do
+		    if not test.result then
+		        print("FAIL", test.name)
+		        if type(test.data) == 'string' and test.data:match("%z") then
+		            print("", (test.data:gsub("%z", ".")))
+		        else
+		            print("",     test.data)
+		        end
+		    end
+		end
+		
+		print("\tTotal: ", #group)
+		print("\tFailed:", failed)
+		print()
+	end
+end
+
+-- determine host endianness - HACK HACK HACK
+function test.bigendian()
+    return string.byte(string.dump(function() end)) == 0x00
+end
+
+return test
