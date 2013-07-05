1e7f45460bfa6d18ddfc64fa810492768b7501d9
Ben Kelly <bk@ancilla.ca>
2013-07-04 21:31:09 -0400

    Add more tests for error reporting

--- vstruct/test/error.lua
+++ vstruct/test/error.lua
@@ -33,8 +33,35 @@ E("invalid-arg-u3", "invalid data argument to vstruct.unpack", vstruct.unpack, "
 E("invalid-arg-p3", "invalid data argument to vstruct.pack", vstruct.pack, "@0", nil, "1234")
 
 -- format string is ill-formed
+-- note that the empty format string is well-formed, does nothing, and returns/accepts the empty table
+E("invalid-format-number", "expected.*, got EOF", vstruct.compile, "4")
+E("invalid-format-}", "expected.* or io specifier, got }", vstruct.compile, "}")
+E("invalid-format-)", "expected.* or io specifier, got %)", vstruct.compile, ")")
+E("invalid-format-]", "expected.* or io specifier, got %]", vstruct.compile, "]")
+E("invalid-format-{", "expected.*, got EOF", vstruct.compile, "{")
+E("invalid-format-(", "expected.*, got EOF", vstruct.compile, "(")
+E("invalid-format-[", "expected.*, got EOF", vstruct.compile, "[")
+E("invalid-format-*", "expected.*or io specifier, got %*", vstruct.compile, "*4")
+E("invalid-format-no-width", "format requires a width", vstruct.compile, "u u4")
+
 -- format string is well-formed but nonsensical
-  -- zero-length fields
-  -- bitfield width doesn't match width of contents
-  -- no support for format
+-- note that empty groups and tables and zero-length repeats make it easier to dynamically construct format strings, and are thus allowed
+E("bad-format-no-support", "no support for format 'q'", vstruct.compile, "q1")
+E("bad-format-small-bitpack", "bitpack contents are smaller than containing bitpack", vstruct.compile, "[1|u4]")
+E("bad-format-large-bitpack", "bitpack contents are larger than containing bitpack", vstruct.compile, "[1|u16]")
+
+-- io format width checking occurs on a format-by-format basis
+E("bad-format-size-missing-f", "only supports widths 4", vstruct.compile, 'f')
+E("bad-format-size-wrong-f", "only supports widths 4", vstruct.compile, 'f1')
+E("bad-format-fraction-p", "format requires a fractional%-part width", vstruct.compile, 'p4')
+-- note that s and z can be used either with or without a width specifier
+local sized_formats = "abcimpux@+-"
+local plain_formats = "<>="
+for format in sized_formats:gmatch(".") do
+    E("bad-format-size-missing-"..format, "format requires a width", vstruct.compile, format)
+end
+for format in plain_formats:gmatch(".") do
+    E("bad-format-size-present-"..format, "is an endianness control, and does not have width", vstruct.compile, format.."1")
+end
+
 -- input table doesn't match format string
