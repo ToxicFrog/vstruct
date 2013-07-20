438d262bc5b88b69b4b076ce20d3fcfcbc97c0dc
Ben Kelly <bk@ancilla.ca>
2013-07-20 13:51:23 -0400

    Update changelog and _VERSION for 1.1.4 release

--- vstruct/init.lua
+++ vstruct/init.lua
@@ -10,7 +10,7 @@ local vstruct = {}
 package.loaded.vstruct = {}
 
 vstruct._NAME = "vstruct"
-vstruct._VERSION = "1.1.3"
+vstruct._VERSION = "1.1.4"
 vstruct._M = vstruct
 
 vstruct.cursor = require "vstruct.cursor"
